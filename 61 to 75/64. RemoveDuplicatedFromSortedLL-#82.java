21e7de1bf22408003068306ea6859f2581c9fe2f
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-06-19 17:33:26 +0530

    Create 64. RemoveDuplicatedFromSortedLL-#82.java

---
+++ 61 to 75/64. RemoveDuplicatedFromSortedLL-#82.java
@@ -0,0 +1,22 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode curr = head;
+
+        while(curr != null) {
+            while(curr.next != null && curr.val == curr.next.val) {
+                curr = curr.next;
+            }
+            if(prev.next == curr) {
+                prev = prev.next;
+            } else {
+                prev.next = curr.next;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
