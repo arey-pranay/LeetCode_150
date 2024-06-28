b746ce656f5b6d2362653ea3851ce39b0ec18870
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-06-28 18:05:20 +0530

    Update 23. FirstOccurence-#28.java

--- 16 to 30/23. FirstOccurence-#28.java
+++ 16 to 30/23. FirstOccurence-#28.java
@@ -1,5 +1,25 @@
+// class Solution {
+//     public int strStr(String haystack, String needle) {
+//         return haystack.indexOf(needle);
+//     }
+// }
+
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        int hSz = haystack.length();
+        int nSz = needle.length();
+        //i is no. of elements that we've traversed through the haystack before entering the check process
+        //j is np. of elements matched
+        if(hSz < nSz)
+            return -1;
+        for(int i=0;i<=hSz-nSz;i++){
+            int j=0;
+            while(j<nSz && haystack.charAt(i+j)==needle.charAt(j))
+                j++;
+            if(j == nSz){
+                return i;
+            }
+        }
+        return -1;
     }
 }
